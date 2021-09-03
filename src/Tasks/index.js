d6d4bad185d655d4bfae460a83a45fc1e89a8582
Patryk Krawczyk <patrick36212@gmail.com>
2021-09-03 15:12:56 +0200

    add key to tasks

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 const Tasks = ({ tasks, hideDoneTasks }) => (
     <ul className="list">
         {tasks.map(task => (
-            <li className={`${task.done && hideDoneTasks ? "list__item--hidden" : ""} list__item`}>
+            <li key={task.id} className={`${task.done && hideDoneTasks ? "list__item--hidden" : ""} list__item`}>
                 <button className={`${task.done ? "button--done" : ""} button button--complete`}><i className="material-icons md-24">task_alt</i></button>
                 <span className={`${task.done ? "list__text--done" : ""} list__text`}>{task.content}</span>
                 <button className="button button--delete"><i className="material-icons md-24">delete</i></button> 
