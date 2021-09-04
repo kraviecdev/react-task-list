dbd274bd1fe7108dfbf10fb4c1d2e1bb3deff73e
Patryk Krawczyk <patrick36212@gmail.com>
2021-09-04 03:05:37 +0200

    hide done tasks function

--- src/App.js
+++ src/App.js
@@ -5,20 +5,43 @@ import Section from "./Section";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
 import Container from "./Container";
+import { useState } from "react";
 
 let tasks = [
   { id: 1, content: "Transfer project to React", done: true, },
   { id: 2, content: "Add functions to project", done: false, },
 ];
 
-let hideDoneTasks = false;
 
 function App() {
+  let [hideDoneTasks, setHideDoneTask] = useState(false);
+  let [markAllTasksDone] = useState(false)
+
+  const toggleHideDone = () => {
+    setHideDoneTask(hideDoneTasks = !hideDoneTasks);
+  };
+
   return (
     <Container>
-      <Header title="Task List"/>
-      <Section title="Add task" body={<Form />} /> 
-      <Section title="List of tasks" body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />} extraButtons={<Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />} />
+      <Header title="Task List" />
+      <Section
+        title="Add task"
+        body={<Form />}
+      />
+      <Section
+        title="List of tasks"
+        body={
+          <Tasks
+            tasks={tasks}
+            hideDoneTasks={hideDoneTasks}
+          />}
+        extraButtons={
+          <Buttons
+            tasks={tasks}
+            hideDoneTasks={hideDoneTasks}
+            toggleHideDone={toggleHideDone}            
+          />}
+      />
       <Footer name="Patryk Krawczyk" year="2021" />
     </Container>
   );
--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,13 +1,24 @@
 import "./style.css";
 
-const Buttons = ({ tasks, hideDoneTasks }) => {
+const Buttons = ({ tasks, hideDoneTasks, toggleHideDone }) => {
+
     if (tasks.length === 0) {
         return null;
     }
     return (
         <div className="section__buttons">
-            <button disabled={(tasks.every(({ done }) => done))} className="button button--selectAll">Select all</button>
-            <button disabled={(tasks.some(({ done }) => done)) && hideDoneTasks} className="button button--hideDone">{hideDoneTasks ? "Show" : "Hide"} done</button>
+            <button
+                disabled={(tasks.every(({ done }) => done))}
+                className="button button--selectAll"
+            >
+                Mark all as done
+            </button>
+            <button
+                onClick={toggleHideDone}
+                className="button button--hideDone"
+            >
+                {hideDoneTasks ? "Show" : "Hide"} done
+            </button>
         </div>
     );
 };
