916bcaf400838b71b34f821de6ac0841788fba9a
Patryk Krawczyk <patrick36212@gmail.com>
2021-09-16 10:05:50 +0200

    add local storage for tasks

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,39 +1,47 @@
+import { useEffect } from "react";
 import "./style.css";
 
-const Tasks = ({ tasks, hideDoneTasks, removeTask, toggleDoneTask }) => (
-    <ul className="list">
-        {tasks.map(task => (
-            <li
-                key={task.id}
-                className={`${task.done && hideDoneTasks ? "list__item--hidden" : ""} list__item`}
-            >
-                <button
-                    onClick={() => toggleDoneTask(task.id)}
-                    className={`${task.done ? "button--done" : ""} button button--complete`}
+const Tasks = ({ tasks, hideDoneTasks, removeTask, toggleDoneTask }) => {
+
+    useEffect(() => {
+        localStorage.setItem("storedTasks", JSON.stringify(tasks));
+    }, [tasks]);
+
+    return (
+        <ul className="list">
+            {tasks.map(task => (
+                <li
+                    key={task.id}
+                    className={`${task.done && hideDoneTasks ? "list__item--hidden" : ""} list__item`}
                 >
-                    <i
-                        className="material-icons md-24"
+                    <button
+                        onClick={() => toggleDoneTask(task.id)}
+                        className={`${task.done ? "button--done" : ""} button button--complete`}
                     >
-                        task_alt
-                    </i>
-                </button>
-                <span
-                    className={`${task.done ? "list__text--done" : ""} list__text`}>
-                    {task.content}
-                </span>
-                <button
-                    onClick={() => removeTask(task.id)}
-                    className="button button--delete"
-                >
-                    <i
-                        className="material-icons md-24"
+                        <i
+                            className="material-icons md-24"
+                        >
+                            task_alt
+                        </i>
+                    </button>
+                    <span
+                        className={`${task.done ? "list__text--done" : ""} list__text`}>
+                        {task.content}
+                    </span>
+                    <button
+                        onClick={() => removeTask(task.id)}
+                        className="button button--delete"
                     >
-                        delete
-                    </i>
-                </button>
-            </li>
-        ))}
-    </ul >
-);
+                        <i
+                            className="material-icons md-24"
+                        >
+                            delete
+                        </i>
+                    </button>
+                </li>
+            ))}
+        </ul >
+    );
+};
 
 export default Tasks;
