1b5b4e028aec119d3bf6f845f2c978a952e2b012
Patryk Krawczyk <patrick36212@gmail.com>
2022-10-07 12:48:44 +0200

    install and implement react-router

--- src/App.js
+++ src/App.js
@@ -2,23 +2,45 @@ import React from "react";
 import Header from "./common/Header";
 import Footer from "./common/Footer"
 import Tasks from "./features/tasks/Tasks";
+import Menu from "./common/Menu";
+import Author from "./features/author/Author";
 
 import { useSelector } from "react-redux";
 import { selectIsDarkTheme } from "./common/ThemeSwitch/themeSlice";
 import { dark, light } from "./theme";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./GlobalStyle";
+import { Route, HashRouter, Switch } from "react-router-dom";
+import { StyledNavLink } from "./common/Menu/styled";
+
 
 const App = () => {
     const isDarkTheme = useSelector(selectIsDarkTheme);
 
-        return (
-        <ThemeProvider theme={isDarkTheme ? dark : light}>
-            <GlobalStyle />
-            <Header title={"Tasks List"} />
-            <Tasks />
-            <Footer />
-        </ThemeProvider>
+    return (
+        <HashRouter>
+            <ThemeProvider theme={isDarkTheme ? dark : light}>
+                <GlobalStyle />
+                <Header
+                    title={"Tasks List"}
+                    navigation={
+                        <Menu
+                            home={<StyledNavLink to={"/"}>Home</StyledNavLink>}
+                            about={<StyledNavLink to={"/author"}>Author</StyledNavLink>}
+                        />
+                    }
+                />
+                <Switch>
+                    <Route path={"/author"} >
+                        <Author />
+                    </Route>
+                    <Route path={"/"} >
+                        <Tasks />
+                    </Route>
+                </Switch>
+                <Footer />
+            </ThemeProvider>
+        </HashRouter>
     )
 };
 
