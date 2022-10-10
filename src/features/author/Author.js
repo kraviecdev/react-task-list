8dae4d575e32763f83b1ce5b4cdde79f134ab097
Patryk Krawczyk <patrick36212@gmail.com>
2022-10-10 19:58:29 +0200

    create description for Author and App info section

--- src/features/author/Author.js
+++ src/features/author/Author.js
@@ -1,13 +1,23 @@
 import Container from "../../common/Container";
+import Paragraph from "../../common/Paragraph";
 import Section from "../../common/Section";
-import About from "./About";
+import { StyledParagraphLink } from "../../common/Paragraph/styled";
 
 const Author = () => {
     return (
         <Container>
             <Section
-                title="Author"
-                body={<About />}
+                title={"Author"}
+                body={
+                    <Paragraph description={
+                        <span>I am a Front-End Developer out of love and passion. I am trying to spent every spare time to have more practise in it. &#9994;
+                            <br />Currently I am working at my <strong>Personal homepage, Tasks List Application & Layout for Brand Page.</strong>
+                            <br />Mean time I am still learning <strong>Redux, React Router and some Figma</strong> Sometimes I catch myself at checking the source code of applications or pages just to see how others made it. &#128064;
+                            <br />When I start to code, time doesn't metter &#9201; Sometimes I am spending the whole night just to make sure that submit button have look and properties I want &#128076;
+                            <br />Personally I am very polite and open person. I love to watch films & TV series. My last favourite is <q><em>The good doctor</em></q> &#128138;
+                            <br />Now you know something about me, if you want to know more about the application got to <StyledParagraphLink href="/react-task-list#/app_info">Application Info</StyledParagraphLink> section. Contact info in in the Footer at the bottom of the page. That one below is me &#128071;
+                        </span>} />
+                }
             />
         </Container>
     );
--- src/features/tools/ApplicationInfo.js
+++ src/features/tools/ApplicationInfo.js
@@ -1,15 +1,30 @@
 import Container from "../../common/Container";
 import Section from "../../common/Section";
+import Paragraph from "../../common/Paragraph";
+import AppInfo from "./AppInfo";
+import { StyledParagraphLink } from "../../common/Paragraph/styled";
+
 
 const ApplicationInfo = () => {
-    return(
+    return (
         <Container>
-            <Section 
+            <Section
                 title={"Application Info"}
-                body={<p>tutaj będzie strona z narzędziami</p>}
+                body={
+                    <Paragraph
+                        description={
+                            <span>
+                                This Application was made during the <em>YouCode</em> course as one of the project. &#128187;
+                                <br />First project was simple and includes HTML, CSS and Vanilla JS. You can spot it <StyledParagraphLink href="https://patrick36212.github.io/to-do-list/" target="_blank">&#128073; here</StyledParagraphLink>
+                                <br />This is more extended version. It contains more advanced techniques learned by months of study and practise. &#128640;
+                                <br />Open Source libraries used to make Tasks List application:
+                            </span>
+                        } />
+                }
             />
+            <AppInfo />
         </Container>
-    )
+    );
 };
 
 export default ApplicationInfo;
