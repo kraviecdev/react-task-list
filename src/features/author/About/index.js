aa86b1740af48565f3b88b9628164ed6539817c4
Patryk Krawczyk <patrick36212@gmail.com>
2022-10-17 21:01:14 +0200

    About component

---
+++ src/features/author/About/index.js
@@ -0,0 +1,16 @@
+import { StyledSection } from "../../../common/Section/styled";
+import personalImage from "./PK.png";
+import { StyledAuthorInfo, StyledImg } from "./styled";
+
+const About = () => {
+    return (
+        <StyledSection additionalSection>
+            <StyledImg src={personalImage} alt="Patryk Krawczyk - the author" />
+            <StyledAuthorInfo>Patryk Krawczyk - Front-End Developer but also SOC Analyst.
+                <br />CyberSecurity is my second passion.
+                <br />Open to learn new technologies but also secur techniques.</StyledAuthorInfo>
+        </StyledSection>
+    );
+}
+
+export default About;
