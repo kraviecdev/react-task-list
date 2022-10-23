434f9c03deaa9c4d72152a8d2d74752217d42551
Patryk Krawczyk <patrick36212@gmail.com>
2022-10-23 19:53:40 +0200

    change in description

--- src/features/tools/ApplicationInfo.js
+++ src/features/tools/ApplicationInfo.js
@@ -15,9 +15,9 @@ const ApplicationInfo = () => {
                         description={
                             <span>
                                 This Application was made during the <em>YouCode</em> course as one of the project. &#128187;
-                                <br />First project was simple and includes HTML, CSS and Vanilla JS. You can spot it <StyledParagraphLink href="https://patrick36212.github.io/to-do-list/" target="_blank">&#128073; here</StyledParagraphLink>
+                                <br />First project was simple and includes HTML, CSS and JavaScript. You can spot it <StyledParagraphLink href="https://patrick36212.github.io/to-do-list/" target="_blank">&#128073; here</StyledParagraphLink>
                                 <br />This is more extended version. It contains more advanced techniques learned by months of study and practise. &#128640;
-                                <br />Open Source libraries used to make Tasks List application:
+                                <br />Below are Open Source libraries used to make Tasks List application and its short info:
                             </span>
                         } />
                 }
