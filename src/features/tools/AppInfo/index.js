d8d2ca761e6540fe3df7485f5b554f5148da2786
Patryk Krawczyk <patrick36212@gmail.com>
2022-10-23 19:18:37 +0200

    add libraries & descriptions

--- src/features/tools/AppInfo/index.js
+++ src/features/tools/AppInfo/index.js
@@ -1,6 +1,6 @@
 import { StyledSection } from "../../../common/Section/styled";
 import Library from "../Library/Index";
-import { ReactIcon } from "../Library/styled";
+import { PagesIcon, ReactIcon, ReduxIcon, RouterIcon, StyledComponentsIcon, StyledLibraryInfo } from "../Library/styled";
 import { StyledLibrariesContainer } from "./styled";
 
 
@@ -11,7 +11,64 @@ const AppInfo = () => {
                 <Library
                     name={"React"}
                     icon={<ReactIcon />}
-                    description={"A JavaScript library for building user interfaces"}
+                    paragraph={
+                        <StyledLibraryInfo>
+                            A JavaScript library for building user interfaces.
+                            <br />In application:
+                            <br />&#9654; Components
+                            <br />&#9654; Properties
+                            <br />&#9654; useRef
+                        </StyledLibraryInfo>}
+                />
+                <Library
+                    name={"React-Redux"}
+                    icon={<ReduxIcon />}
+                    paragraph={
+                        <StyledLibraryInfo>
+                            A Predictable State Container for JS Apps.
+                            <br />In application:
+                            <br />&#9654; Reducers
+                            <br />&#9654; useSelector
+                            <br />&#9654; useDispatch
+                            <br />&#9654; Saga
+                        </StyledLibraryInfo>
+                    }
+                />
+                <Library
+                    name={"React-Router"}
+                    icon={<RouterIcon />}
+                    paragraph={
+                        <StyledLibraryInfo>
+                            A fully-featured client and server-side routing library for React.
+                            <br />In application:
+                            <br />&#9654; HashRouter
+                            <br />&#9654; Custom NavLink
+                        </StyledLibraryInfo>
+                    }
+                />
+                <Library
+                    name={"Github Pages"}
+                    icon={<PagesIcon />}
+                    paragraph={
+                        <StyledLibraryInfo>
+                            A static site hosting service that takes HTML, CSS, and JavaScript files straight from a repository on GitHub.
+                            <br />In application:
+                            <br />&#9654; Deployment
+                        </StyledLibraryInfo>
+                    }
+                />
+                <Library
+                    name={"StyledComponents"}
+                    icon={<StyledComponentsIcon />}
+                    paragraph={
+                        <StyledLibraryInfo>
+                            A library for React & ReactNative that allows to use component-level styles in applications.
+                            <br />In application:
+                            <br />&#9654; Styled components
+                            <br />&#9654; Theme & GlobalStyle
+                            <br />&#9654; Icons
+                        </StyledLibraryInfo>
+                    }
                 />
             </StyledLibrariesContainer>
         </StyledSection>
