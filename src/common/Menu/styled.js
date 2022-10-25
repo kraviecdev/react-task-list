fe8e1d0a2ce7e8f6ae4bad8be4eb03032e72ac02
Patryk Krawczyk <patrick36212@gmail.com>
2022-10-25 18:38:31 +0200

    better view in mobile

--- src/common/Menu/styled.js
+++ src/common/Menu/styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 export const StyledMenu = styled.nav`
-    padding: 10px;
+    padding: 20px 10px;
     display: flex;
     justify-content: space-between;
     flex-grow: 1;
