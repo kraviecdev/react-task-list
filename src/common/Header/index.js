23c8cbb2b183d07bd02ee03b1c92302b84b5777b
Patryk Krawczyk <patrick36212@gmail.com>
2022-10-26 18:22:01 +0200

    add header in mobile only when menu closed

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -1,10 +1,16 @@
+import { useSelector } from "react-redux";
+import { selectIsMenuOpen } from "../Menu/menuSlice";
 import { StyledHeader, StyledTitle } from "./styled";
 
-const Header = ({ title, navigation }) => (
-    <StyledHeader>
-        <StyledTitle>{title}</StyledTitle>
-        {navigation}
-    </StyledHeader>
-);
+const Header = ({ title, navigation }) => {
+    const isMenuOpen = useSelector(selectIsMenuOpen); 
+
+    return (
+        <StyledHeader>
+            <StyledTitle active={!isMenuOpen} >{title}</StyledTitle>
+            {navigation}
+        </StyledHeader>
+    )
+};
 
 export default Header;
--- src/common/Header/styled.js
+++ src/common/Header/styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { css } from "styled-components";
 
 export const StyledHeader = styled.header`
     display: flex;
@@ -7,10 +8,19 @@ export const StyledHeader = styled.header`
     padding: 12px;
     background-color: inherit;
     margin: 0 auto;
+    min-height: 61px;
     width: 100%;
     max-width: 1420px;
 `;
 
 export const StyledTitle = styled.h1`
-    margin: 0;
+    display: none;
+
+    @media(max-width: ${({ theme }) => theme.breakpoints.medium}px){
+        ${({ active }) => active && css`
+        margin: 0;
+        display: block;
+    `}
+    }
+    
 `;
