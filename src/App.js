b19f7cf9903c6b30985bbeab9055106838951a5a
Patryk Krawczyk <patrick36212@gmail.com>
2022-10-28 14:05:17 +0200

    change path for exact and add path to taks by id

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Footer from "./common/Footer"
 import Tasks from "./features/tasks/Tasks";
 import Menu from "./common/Menu";
 import ApplicationInfo from "./features/tools/ApplicationInfo";
-import About from "./features/author/About";
+import About from "./features/about/About";
+import TaskInfo from "./features/tasks/TaskInfo";
 
 import { useSelector } from "react-redux";
 import { selectIsDarkTheme } from "./common/ThemeSwitch/themeSlice";
@@ -33,14 +34,17 @@ const App = () => {
                     }
                 />
                 <Switch>
-                    <Route path={"/author"} >
+                    <Route exact path={"/"} >
+                        <Tasks />
+                    </Route>
+                    <Route path={"/task/:id"} >
+                        <TaskInfo />
+                    </Route>
+                    <Route exact path={"/author"} >
                         <About />
                     </Route>
-                    <Route path={"/app_info"}>
+                    <Route exact path={"/app_info"}>
                         <ApplicationInfo />
-                    </Route>
-                    <Route path={"/"} >
-                        <Tasks />
                     </Route>
                 </Switch>
                 <Footer />
