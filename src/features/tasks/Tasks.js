a899afeb6fa20e614873f678f3e33d77a14279ab
Patryk Krawczyk <patrick36212@gmail.com>
2022-11-01 18:40:51 +0100

    add search section

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -3,6 +3,7 @@ import Section from "../../common/Section";
 import Container from "../../common/Container";
 import TasksList from "./TasksList";
 import Buttons from "../../common/Buttons";
+import Search from "../tasks/Search";
 import { StyledButton, StyledButtonSection } from "../../common/Buttons/styled";
 import { useDispatch } from "react-redux";
 import { fetchExampleTasks } from "./tasksSlice";
@@ -25,6 +26,10 @@ const Tasks = () => {
             </StyledButton>
           </StyledButtonSection>
         }
+      />
+       <Section
+        title="Search"
+        body={<Search />}
       />
       <Section
         title="List of tasks"
