9ca5e5f4c007179a02d94e2b9182b08b8569b158
patrick36212 <patrick36212@gmail.com>
2024-02-07 12:07:43 +0100

    refactor burger styled component

--- src/common/Burger/index.js
+++ src/common/Burger/index.js
@@ -1,26 +1,21 @@
 import { useDispatch, useSelector } from "react-redux";
 import { selectIsMenuOpen, toggleMenuState } from "../Menu/menuSlice";
-import { StyledBurger, StyledBurgerSwitch } from "./styled";
+import { StyledBurger } from "./styled";
 
 const Burger = () => {
-    const isMenuOpen = useSelector(selectIsMenuOpen);
-    const dispatch = useDispatch();
+  const isMenuOpen = useSelector(selectIsMenuOpen);
+  const dispatch = useDispatch();
 
-    return (
-        <StyledBurger
-            onClick={() => dispatch(toggleMenuState())}
-        >
-            <StyledBurgerSwitch
-                active={isMenuOpen}
-            />
-            <StyledBurgerSwitch
-                active={isMenuOpen}
-            />
-            <StyledBurgerSwitch
-                active={isMenuOpen}
-            />
-        </StyledBurger>
-    )
+  return (
+    <StyledBurger
+      active={isMenuOpen}
+      onClick={() => dispatch(toggleMenuState())}
+    >
+      <div />
+      <div />
+      <div />
+    </StyledBurger>
+  );
 };
 
 export default Burger;
--- src/common/Burger/styled.js
+++ src/common/Burger/styled.js
@@ -2,47 +2,47 @@ import styled from "styled-components";
 
 export const StyledBurger = styled.button`
     display: none;
-    
-    @media(max-width: ${({ theme }) => theme.breakpoints.medium}px) {
-      position: absolute;
-      top: 14px;
-      right: 14px;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-around;
-      width: 32px;
-      height: 32px;
-      background: transparent;
-      border: none;
-      cursor: pointer;
-      padding: 0;
-      z-index: 2;
-      
-      &:focus {
-          outline: none;
-      }
+
+    div {
+        width: 32px;
+        height: 4px;
+        background: ${({ theme }) => theme.colors.colorPrimary};
+        border-radius: 10px;
+        transition: all 0.3s linear;
+        position: relative;
+        transform-origin: 1px;
+
+        &:first-of-type {
+            transform: ${({ active }) => active ? 'rotate(45deg)' : 'rotate(0)'};
+        }
+
+        &:nth-of-type(2) {
+            opacity: ${({ active }) => active ? '0' : '1'};
+            transform: ${({ active }) => active ? 'translateX(20px)' : 'translateX(0)'};
+        }
+
+        &:nth-of-type(3) {
+            transform: ${({ active }) => active ? 'rotate(-45deg)' : 'rotate(0)'};
+        }
+    }
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
+        position: absolute;
+        top: 14px;
+        right: 14px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+        width: 32px;
+        height: 32px;
+        background: transparent;
+        border: none;
+        cursor: pointer;
+        padding: 0;
+        z-index: 2;
+
+        &:focus {
+            outline: none;
+        }
     }
 `;
-
-export const StyledBurgerSwitch = styled.div`
-    width: 32px;
-    height: 4px;
-    background: ${({theme}) => theme.colors.colorPrimary};
-    border-radius: 10px;
-    transition: all 0.3s linear;
-    position: relative;
-    transform-origin: 1px;
-
-    :first-child {
-      transform: ${({ active }) => active ? 'rotate(45deg)' : 'rotate(0)'};
-    }
-
-    :nth-child(2) {
-      opacity: ${({ active }) => active ? '0' : '1'};
-      transform: ${({ active }) => active ? 'translateX(20px)' : 'translateX(0)'};
-    }
-
-    :nth-child(3) {
-      transform: ${({ active }) => active ? 'rotate(-45deg)' : 'rotate(0)'};
-    }
-`
