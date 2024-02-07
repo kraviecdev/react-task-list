09e90b118e64519809c928193b21760659aaddf8
patrick36212 <patrick36212@gmail.com>
2024-02-07 12:10:24 +0100

    change implementation to react 18.+

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import App from './App';
-import { Provider } from 'react-redux';
-import store from './store';
+import App from './core/App';
+import {Provider} from 'react-redux';
+import store from './core/store';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.createRoot(document.getElementById('root')).render(
+        <Provider store={store}>
+            <App/>
+        </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
