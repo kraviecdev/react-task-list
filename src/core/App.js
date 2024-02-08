0f042ca4bc482025e2e476bc731289e438531f90
patrick36212 <patrick36212@gmail.com>
2024-02-08 20:43:44 +0100

    implement newer version app

---
+++ src/core/App.js
@@ -0,0 +1,22 @@
+import { useSelector } from "react-redux";
+import { selectIsDarkTheme } from "../common/Header/ThemeSwitch/themeSlice";
+import { ThemeProvider } from "styled-components";
+import { dark, light } from "../theme/theme";
+import { GlobalStyle } from "../theme/GlobalStyle";
+import { RouterProvider } from "react-router-dom";
+import { routes } from "./routes";
+
+
+
+const App = () => {
+  const isDarkTheme = useSelector(selectIsDarkTheme);
+
+  return (
+    <ThemeProvider theme={isDarkTheme ? dark : light}>
+      <GlobalStyle />
+      <RouterProvider router={routes} />
+    </ThemeProvider>
+  );
+};
+
+export default App;
