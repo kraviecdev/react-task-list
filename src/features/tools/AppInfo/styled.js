a269ef5b4422762e877501e72cc732c8256501c0
patrick36212 <patrick36212@gmail.com>
2024-02-12 18:10:34 +0100

    del unused files

--- src/features/tools/AppInfo/styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const StyledLibrariesContainer = styled.div`
-    padding: 10px;
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-`;
