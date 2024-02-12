79fb0ff1d27b9f02878de3552312441679d0aca2
patrick36212 <patrick36212@gmail.com>
2024-02-12 18:10:51 +0100

    add styled to root component

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -3,8 +3,8 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
     html {
         background-color: ${({ theme }) => theme.colors.backgroundSecondary};
-        color: ${({theme}) => theme.colors.colorPrimary};
-        box-sizing: border-box;  
+        color: ${({ theme }) => theme.colors.colorPrimary};
+        box-sizing: border-box;
     }
 
     *, ::after, ::before {
@@ -15,5 +15,11 @@ export const GlobalStyle = createGlobalStyle`
         font-family: 'Lato', sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
+    }
+
+    #root {
+        display: flex;
+        flex-direction: column;
+        min-height: 100dvh;
     }
 `;
