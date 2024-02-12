cfcd0088c12fd122e75eaad3325beebf7848ed57
patrick36212 <patrick36212@gmail.com>
2024-02-12 18:15:04 +0100

    change function component to a StyledComponent

--- src/common/Paragraph/index.js
+++ src/common/Paragraph/index.js
@@ -1,7 +1,31 @@
-import { StyledParagraph } from "./styled";
+import styled, { css } from "styled-components";
 
-const Paragraph = ({ description }) => (
-    <StyledParagraph>{description}</StyledParagraph>
-);
+export const Paragraph = styled.p`
+    margin: 0;
+    text-align: justify;
+    font-size: 20px;
+    white-space: break-spaces;
+    line-height: 32px;
 
-export default Paragraph;
+    span {
+        font-weight: bold;
+        color: ${({ theme }) => theme.colors.buttonPrimary};
+    }
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
+        font-size: 14px;
+        line-height: 26px;
+    }
+
+    ${({ author }) => author && css`
+        align-self: flex-end;
+    `}
+
+    ${({ footer }) => footer && css`
+        width: 60%;
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
+            width: 100%;
+        }
+    `}
+`;
--- src/common/Section/index.js
+++ src/common/Section/index.js
@@ -1,17 +1,37 @@
-import { StyledSection, StyledSectionHeader, StyledSubheader } from "./styled";
+import styled, { css, keyframes } from "styled-components";
 
-const Section = ({ title, additionalContent, body }) => {
-    return (
-        <>
-            <StyledSection>
-                <StyledSectionHeader>
-                    <StyledSubheader>{title}</StyledSubheader>
-                    {additionalContent}
-                </StyledSectionHeader>
-                {body}
-            </StyledSection>
-        </>
-    )
-};
+const fadeInLeft = keyframes`
+    from {
+        transform: translate3d(-100%, 0, 0);
+    }
+    to {
+        transform: none;
+    }
+`;
 
-export default Section;
+const fadeInRight = keyframes`
+    from {
+        transform: translate3d(100%, 0, 0);
+    }
+    to {
+        transform: none;
+    }
+`;
+
+export const Section = styled.section`
+    background-color: ${({ theme }) => theme.colors.backgroundPrimary};
+    margin: 20px auto;
+    padding: 26px;
+
+    &:nth-child(odd) {
+        animation: ${fadeInLeft} .7s;
+    }
+
+    &:nth-child(even) {
+        animation: ${fadeInRight} .7s;
+    }
+
+    ${({ transparent }) => transparent && css`
+        background: transparent;
+    `}
+`;
