9a23a722d40bfd079a3f2713d8c198065063354b
patrick36212 <patrick36212@gmail.com>
2024-02-14 13:51:52 +0100

    change styled components names, del duplicate styled component

---
+++ src/common/TasksList/styled.js
@@ -0,0 +1,18 @@
+import styled, { css } from "styled-components";
+
+export const List = styled.ul`
+    padding: 10px;
+    list-style-type: none;
+`;
+
+export const ListItem = styled.li`
+    align-items: center;
+    display: flex;
+    padding: 5px;
+    margin-top: 10px;
+    border-bottom: 1px solid ${({ theme }) => theme.colors.colorSecondary};
+
+    ${({ hidden }) => hidden && css`
+        display: none;
+    `}
+`;
