89e2dbc72b91d8415c3a16e3d8a0c82596df3d73
patrick36212 <patrick36212@gmail.com>
2024-02-14 16:27:20 +0100

    change Wrapper css add search prop

--- src/common/Wrapper/index.js
+++ src/common/Wrapper/index.js
@@ -1,12 +1,26 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 
 export const Wrapper = styled.div`
     display: flex;
-    flex-direction: ${({column}) => column ? "column" : "row"};
+    flex-direction: ${({ column }) => column ? "column" : "row"};
     gap: 12px;
-    width: 100%;
+    width: ${({ inner }) => inner ? "max-content" : "100%"};
     max-width: ${({ theme }) => theme.breakpoints.extraLarge}px;
-    margin: 0 auto;
-    padding: ${({inner}) => inner ? `10px 0` : `12px`};
+    margin: ${({ inner }) => inner ? "0" : "0 auto"};
+
+    img {
+        max-height: 350px;
+        border-radius: 50% 0;
+        filter: drop-shadow(10px 2px 6px gray);
+
+        @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
+            max-height: 150px;
+            border-radius: 50%;
+        }
+    }
+
+    ${({ search }) => search && css`
+        background-color: ${({ theme }) => theme.colors.backgroundSecondary};
+    `}
 `;
