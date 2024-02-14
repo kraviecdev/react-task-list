cfc5dbe6a59df3231b42ca91a24910fb848f0496
patrick36212 <patrick36212@gmail.com>
2024-02-14 16:19:59 +0100

    add css to search svg from styled-icons

---
+++ src/common/Search/styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { Search } from "@styled-icons/bootstrap/Search";
+
+export const SearchIcon = styled(Search)`
+    width: 24px;
+    margin: 10px;
+`;
