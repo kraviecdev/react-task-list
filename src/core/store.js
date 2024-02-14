cf668b1495eba13b43f54125731c7e26c81f2f1a
patrick36212 <patrick36212@gmail.com>
2024-02-14 13:50:27 +0100

    change imports for redux store.js

--- src/core/store.js
+++ src/core/store.js
@@ -1,17 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
-import tasksReducer from '../features/tasks/tasksSlice';
-import themeReducer from '../common/Header/ThemeSwitch/themeSlice';
+import tasksReducer from "../features/tasks/tasksSlice";
+import themeReducer from "../common/Header/ThemeSwitch/themeSlice";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./rootSaga";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
-    reducer: {
-        tasks: tasksReducer,
-        theme: themeReducer,
-    },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware),
+  reducer: {
+    tasks: tasksReducer,
+    theme: themeReducer
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware)
 });
 
 sagaMiddleware.run(rootSaga);
