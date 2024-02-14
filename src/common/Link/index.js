6064864f1c8b2767f1a73fdb041827d399ae662f
patrick36212 <patrick36212@gmail.com>
2024-02-14 13:46:04 +0100

    add NavLink to Link reusable component

--- src/common/Link/index.js
+++ src/common/Link/index.js
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { NavLink } from "react-router-dom";
 
 export const Link = styled.a`
     font-size: 24px;
@@ -7,11 +8,51 @@ export const Link = styled.a`
     color: ${({ theme }) => theme.colors.buttonPrimary};
     transition: color 1s;
 
-    &:hover{
+    &:hover {
         color: ${({ theme }) => theme.colors.colorPrimary};
     }
-    
+
     @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
         font-size: 20px;
     }
+`;
+
+export const StyledNavLink = styled(NavLink)`
+    cursor: pointer;
+    font-size: ${({ content }) => content ? 20 : 32}px;
+    font-weight: bold;
+    text-decoration: none;
+    color: ${({ theme }) => theme.colors.buttonPrimary};
+    transition: .7s;
+
+    &.active {
+        text-decoration: underline;
+        filter: brightness(1.25);
+    }
+;
+
+    &:hover {
+        filter: brightness(1.25);
+    }
+;
+
+    ${({ header }) => header && css`
+        font-size: 32px;
+        color: ${({ theme }) => theme.colors.colorPrimary};
+        text-decoration: none;
+
+        &:hover {
+            color: ${({ theme }) => theme.colors.buttonPrimary};
+        }
+
+        &.active {
+            color: ${({ theme }) => theme.colors.colorPrimary};
+            text-decoration: none;
+        }
+    ;
+    `};
+
+    ${({ taskLink }) => taskLink && css`
+        font-size: 18px;
+    `};
 `;
