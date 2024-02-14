ee5453913eadbcf87e9a639959457cd51c06bd57
patrick36212 <patrick36212@gmail.com>
2024-02-14 13:48:52 +0100

    del keyframes from Section add about prop for around padding

--- src/common/Section/index.js
+++ src/common/Section/index.js
@@ -1,37 +1,14 @@
-import styled, { css, keyframes } from "styled-components";
-
-const fadeInLeft = keyframes`
-    from {
-        transform: translate3d(-100%, 0, 0);
-    }
-    to {
-        transform: none;
-    }
-`;
-
-const fadeInRight = keyframes`
-    from {
-        transform: translate3d(100%, 0, 0);
-    }
-    to {
-        transform: none;
-    }
-`;
+import styled, { css } from "styled-components";
 
 export const Section = styled.section`
     background-color: ${({ theme }) => theme.colors.backgroundPrimary};
     margin: 20px auto;
-    padding: 26px;
-
-    &:nth-child(odd) {
-        animation: ${fadeInLeft} .7s;
-    }
-
-    &:nth-child(even) {
-        animation: ${fadeInRight} .7s;
-    }
 
     ${({ transparent }) => transparent && css`
         background: transparent;
+    `}
+
+    ${({ about }) => about && css`
+        padding: 16px;
     `}
 `;
