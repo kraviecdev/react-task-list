de8050c6ad4bbe75db9473f31ab4e8ee13db6587
patrick36212 <patrick36212@gmail.com>
2024-02-14 13:46:37 +0100

    improve main css

--- src/common/Main/index.js
+++ src/common/Main/index.js
@@ -3,8 +3,13 @@ import styled from "styled-components";
 export const Main = styled.main`
     flex-grow: 1;
     margin: 0 auto;
-    padding: 20px 10px;
+    padding: 20px 0;
+    width: 100%;
     max-width: ${({ theme }) => theme.breakpoints.extraLarge}px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoints.extraLarge}px) {
+        padding: 20px 10px;
+    }
 
     @media (max-width: ${({ theme }) => theme.breakpoints.medium}px) {
         max-width: 100%;
