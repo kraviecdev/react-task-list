dc14b9c6b7dcf22430423db8a8aa37a51e3ad991
patrick36212 <patrick36212@gmail.com>
2024-02-15 11:14:11 +0100

    create simple routes using createBrowserRouter

---
+++ src/core/routes.js
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router-dom";
+import HomeLayout from "../pages/HomeLayout";
+import About from "../pages/About";
+import Tasks from "../pages/Tasks";
+import Task from "../pages/Task";
+
+
+export const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    children: [
+      {
+        index: true,
+        element: <Tasks />
+      },
+      {
+        path: "task/:id",
+        element: <Task />
+      },
+      {
+        path: "about",
+        element: <About />
+      }
+    ]
+  }
+], {
+  basename: "/react-task-list"
+});
